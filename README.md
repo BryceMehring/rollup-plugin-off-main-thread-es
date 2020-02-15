e80bb585c481495c3c551132b620612a7d3ebffe
Bryce Mehring <brycemehring@gmail.com>
2020-02-14 20:58:05 -0600

    Support ES modules

--- README.md
+++ README.md
@@ -1,54 +1,44 @@
-# rollup-plugin-off-main-thread
+# rollup-plugin-off-main-thread-es
 
-Use Rollup with workers and ES6 modules _today_.
+Use Rollup with workers and ES6 modules. Fork of [@surma/rollup-plugin-off-main-thread](https://github.com/surma/rollup-plugin-off-main-thread) supporting es modules.
 
 ```
-$ npm install --save @surma/rollup-plugin-off-main-thread
+$ npm install --save-dev @brycemehring/rollup-plugin-off-main-thread-es
 ```
 
 Workers are JavaScript’s version of threads. [Workers are important to use][when workers] as the main thread is already overloaded, especially on slower or older devices.
-
-This plugin takes care of shimming module support in workers and allows you to use `new Worker()`.
-
-OMT is the result of merging loadz0r and workz0r.
 
 ## Usage
 
 ```js
 // rollup.config.js
-import OMT from "@surma/rollup-plugin-off-main-thread";
+import OffMainThread from '@brycemehring/rollup-plugin-off-main-thread-es';
 
 export default {
-  input: ["src/main.js"],
+  input: ['src/main.js'],
   output: {
-    dir: "dist",
-    // You _must_ use “amd” as your format
-    format: "amd"
+    dir: 'dist',
+    format: 'es',
   },
-  plugins: [OMT()]
+  plugins: [
+    OffMainThread(),
+  ],
 };
 ```
-
-I set up [a gist] to show a full setup with OMT.
 
 ## Options
 
 ```js
-{
+// rollup.config.js
+export default {
   // ...
-  plugins: [OMT(options)];
+  plugins: [OffMainThread(options)];
 }
 ```
 
-- `loader`: A string containing the EJS template for the amd loader. If `undefined`, OMT will use `loader.ejs`.
-- `useEval`: Use `fetch()` + `eval()` to load dependencies instead of `<script>` tags and `importScripts()`. _This is not CSP compliant, but is required if you want to use dynamic imports in ServiceWorker_.
 - `workerRegexp`: A RegExp to find `new Workers()` calls. The second capture group _must_ capture the provided file name without the quotes.
-- `amdFunctionName`: Function name to use instead of AMD’s `define`.
-- `prependLoader`: A function that determines whether the loader code should be prepended to a certain chunk. Should return true if the load is suppsoed to be prepended.
+- `include/exclude`: A valid minimatch pattern, or array of patterns. If options.include is omitted or has zero length, filter will return true by default. Otherwise, an ID must match one or more of the minimatch patterns, and must not match any of the options.exclude patterns.
 
 [when workers]: https://dassur.ma/things/when-workers
-[a gist]: https://gist.github.com/surma/a02db7b53eb3e7870bf539b906ff6ff6
 
----
 
-License Apache-2.0
--- index.js
+++ index.js
@@ -1,57 +1,29 @@
-/**
- * Copyright 2018 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-const { readFileSync } = require("fs");
-const { join } = require("path");
-const ejs = require("ejs");
-const MagicString = require("magic-string");
+const MagicString = require('magic-string');
+const { createFilter } = require('@rollup/pluginutils');
 
 const defaultOpts = {
-  // A string containing the EJS template for the amd loader. If `undefined`,
-  // OMT will use `loader.ejs`.
-  loader: readFileSync(join(__dirname, "/loader.ejs"), "utf8"),
-  // Use `fetch()` + `eval()` to load dependencies instead of `<script>` tags
-  // and `importScripts()`. _This is not CSP compliant, but is required if you
-  // want to use dynamic imports in ServiceWorker_.
-  useEval: false,
   // A RegExp to find `new Workers()` calls. The second capture group _must_
   // capture the provided file name without the quotes.
-  workerRegexp: /new Worker\((["'])(.+?)\1\)/g,
-  // Function name to use instead of AMD’s `define`.
-  amdFunctionName: "define",
-  // A function that determines whether the loader code should be prepended to a
-  // certain chunk. Should return true if the load is suppsoed to be prepended.
-  prependLoader: (chunk, workerFiles) =>
-    chunk.isEntry || workerFiles.includes(chunk.facadeModuleId)
+  workerRegexp: /new Worker\((["'])(.+?)\1/g,
 };
 
 module.exports = function(opts = {}) {
-  opts = Object.assign({}, defaultOpts, opts);
+  opts = {
+    ...defaultOpts,
+    ...opts,
+  };
 
-  opts.loader = ejs.render(opts.loader, opts);
+  const filter = createFilter(opts.include, opts.exclude);
 
-  const prefix = `"${opts.marker}_start" + import(`;
-  const suffix = `) + "${opts.marker}_end"`;
-
-  let workerFiles;
   return {
-    name: "off-main-thread",
-
-    async buildStart() {
-      workerFiles = [];
-    },
+    name: "off-main-thread-es",
 
     async transform(code, id) {
+
+      if (!filter(id)) {
+        return;
+      }
+
       // Copy the regexp as they are stateful and this hook is async.
       const workerRegexp = new RegExp(
         opts.workerRegexp.source,
@@ -72,24 +44,20 @@ module.exports = function(opts = {}) {
 
         const workerFile = match[2];
 
-        if (!/^\.*\//.test(workerFile)) {
-          this.warn(
-            `Paths passed to the Worker constructor must be relative or absolute, i.e. start with /, ./ or ../ (just like dynamic import!). Ignoring "${workerFile}".`
-          );
-          continue;
-        }
+        const resolvedWorkerFile = await this.resolve(workerFile, id);
 
-        const resolvedWorkerFile = await this.resolveId(workerFile, id);
-        workerFiles.push(resolvedWorkerFile);
-        const chunkRefId = this.emitChunk(resolvedWorkerFile);
+        const chunkRefId = this.emitFile({
+          type: 'chunk',
+          id: resolvedWorkerFile.id,
+        });
 
         const workerFileStartIndex = match.index + "new Worker(".length;
-        const workerFileEndIndex = match.index + match[0].length - ")".length;
+        const workerFileEndIndex = match.index + match[0].length;
 
         ms.overwrite(
           workerFileStartIndex,
           workerFileEndIndex,
-          `import.meta.ROLLUP_CHUNK_URL_${chunkRefId}`
+          `import.meta.ROLLUP_FILE_URL_${chunkRefId}`
         );
       }
 
@@ -98,40 +66,5 @@ module.exports = function(opts = {}) {
         map: ms.generateMap({ hires: true })
       };
     },
-
-    renderChunk(code, chunk, outputOptions) {
-      if (outputOptions.format !== "amd") {
-        this.error("You must set output.format to 'amd'");
-        return;
-      }
-      if (outputOptions.banner && outputOptions.banner.length > 0) {
-        this.error(
-          "OMT currently doesn’t work with `banner`. Feel free to submit a PR at https://github.com/surma/rollup-plugin-off-main-thread"
-        );
-        return;
-      }
-      const ms = new MagicString(code);
-
-      // Mangle define() call
-      const id = `./${chunk.fileName}`;
-      ms.remove(0, "define(".length);
-      // If the module does not have any dependencies, it’s technically okay
-      // to skip the dependency array. But our minimal loader expects it, so
-      // we add it back in.
-      if (!code.startsWith("define([")) {
-        ms.prepend("[],");
-      }
-      ms.prepend(`${opts.amdFunctionName}("${id}",`);
-
-      // Prepend loader if it’s an entry point or a worker file
-      if (opts.prependLoader(chunk, workerFiles)) {
-        ms.prepend(opts.loader);
-      }
-
-      return {
-        code: ms.toString(),
-        map: ms.generateMap({ hires: true })
-      };
-    }
   };
 };
